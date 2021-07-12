39912776d67d261bbd400cc3f8b56090ebed5046
Kyle Tully <kjtully@yahoo.com>
2021-07-12 17:01:00 -0500

    bumping to OTel 0.23.0 where possible

--- node/app.js
+++ node/app.js
@@ -14,7 +14,6 @@ app.get("/node-start", (req, res) => {
   console.log("Sending call to Go!");
   axios
     .get(`http://${GO_URL}:${GO_PORT}/node-chain`)
-    //.then(() => axios.get(`http://${GO_URL}:${GO_PORT}/node-chain`))
     .then(result => {
       res.send(result.data);
     })
@@ -23,26 +22,6 @@ app.get("/node-start", (req, res) => {
       res.status(500).send();
     });
 });
-
-/*
-app.get("/middle-tier", (req, res) => {
-  axios
-    .get(`http://localhost:${PORT}/backend`)
-    .then(() => axios.get(`http://localhost:${PORT}/backend`))
-    .then(result => {
-      res.send(result.data);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send();
-    });
-});
-*/
-/*
-app.get("/backend", (req, res) => {
-  res.send("Hello from the backend");
-});
-*/
 
 app.listen(parseInt(PORT, 10), () => {
   console.log(`Listening for requests on http://localhost:${PORT}`);
--- node/tracing.js
+++ node/tracing.js
@@ -1,29 +1,77 @@
 "use strict";
 
-const { LogLevel } = require("@opentelemetry/core");
-const { BasicTracerProvider, SimpleSpanProcessor } = require("@opentelemetry/tracing");
+const opentelemetry = require('@opentelemetry/api');
+
+// Not functionally required but gives some insight what happens behind the scenes
+const { diag, DiagConsoleLogger, DiagLogLevel } = opentelemetry;
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
 const { NodeTracerProvider } = require('@opentelemetry/node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { CollectorTraceExporter } =  require('@opentelemetry/exporter-collector-grpc');
+const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
+const { Resource } = require('@opentelemetry/resources');
+const { ResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
+// Originally, adding serviceName and service.namespace worked here,
+// But it doesn't seem to work anymore.  Instead, added via Resource (below)
 const collectorOptions = {
   serviceName: 'node-chain',
-  url: 'host.docker.internal:4317',
+  url: 'http://host.docker.internal:4317',
   attributes: {
     'service.namespace' : 'kjt-OTel-chain'
   }
 };
 
 // Instantiate Tracer and Exporter
-// Trying out NodeTracerProvider
-//const provider = new BasicTracerProvider();
-const provider = new NodeTracerProvider();
+const provider = new NodeTracerProvider({
+  resource: new Resource({
+    [ResourceAttributes.SERVICE_NAME]: 'node-chain',
+    [ResourceAttributes.SERVICE_NAMESPACE]: 'kjt-OTel-chain',
+  }),
+});
 const exporter = new CollectorTraceExporter(collectorOptions);
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 provider.register();
 
+registerInstrumentations({
+  tracerProvider: provider,
+  instrumentations: [
+    //HttpInstrumentation: {
+    new HttpInstrumentation({
+      requestHook: (span, request) => {
+        const spanAttrs = span.attributes;
+        var spanName = "";
+        for (const property in spanAttrs) {
+          //console.log(`${property}: ${spanAttrs[property]}`);
+          if (property === "http.method") {
+            //console.log("Found HTTP method! It equals " + spanAttrs[property]);
+            spanName += spanAttrs[property] + " ";
+          } else if (property === "http.route") {
+            //console.log("Found HTTP route! It equals " + spanAttrs[property]);
+            spanName += spanAttrs[property];
+          }
+        }
+        console.log(`Creating Span with derived name: \"${spanName}\"`);
+        span.updateName(spanName);
+      },
+    }),
+  ],
+});
+
+/*
+          if (property === "http.method") {
+
+            method = spanAttrs[property]
+          } else if (property === "http.route") {
+            url = spanAttrs[property]
+          }
+*/
+
 // register and load old default plugins (?)
 // Disabling express plugin to test - the result is much cleaner, prob won't turn it back on
+/*
 registerInstrumentations({
   tracerProvider: provider,
   instrumentations: [
@@ -48,5 +96,6 @@ registerInstrumentations({
     }
   ]
 });
+*/
 
 console.log("Node.js Tracing Initialized!");
